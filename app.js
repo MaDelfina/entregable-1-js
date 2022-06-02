4ca7ec52165004b7751ded3ab9386611017c52d1
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2022-06-02 16:54:24 -0300

    desafio entregable-1

---
+++ app.js
@@ -0,0 +1,14 @@
+//Proyecto: Bikinis | Bikini.twins
+
+//Teniendo en cuenta que es usuario elige 3 productos, sumar todos los precios y ver el total.
+
+//enteriza = 3000;
+//corpiño = 1500;
+//bombacha = 1500;
+
+const suma = (enteriza, corpiño, bombacha) => {
+    let resultado;
+    resultado = enteriza + corpiño + bombacha
+    return resultado}
+
+console.log(suma(3000, 1500, 1500))
