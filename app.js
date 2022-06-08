7fc6a69bf0508f7bbc5399d4f464f9cd155bc66e
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2022-06-08 09:57:00 -0300

    corecciones

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 //Proyecto: Bikinis | Bikini.twins
 
-//Teniendo en cuenta que es usuario elige 3 productos, sumar todos los precios y ver el total.
+//Teniendo en cuenta que es usuario elige entre 3 productos, sumar iva y hacer dscuento.
 
-//enteriza = 3000;
-//corpiño = 1500;
-//bombacha = 1500;
+prompt("Los productos son: Enteriza: $3000, corpiño: $1500 y bombacha: $1500")
 
-const suma = (enteriza, corpiño, bombacha) => {
-    let resultado;
-    resultado = enteriza + corpiño + bombacha
-    return resultado}
+function producto(enteriza, corpiño, bombacha) {
 
-console.log(suma(3000, 1500, 1500))
+    switch (operacion) {
+        case "enteriza":
+            alert("El total del producto es " + precioIva)
+            break;
+        case "corpiño" || "bombacha":
+            alert("El total del producto es " + precioIva2)
+            break;
+        default:
+            "otro";
+            break;
+    }
+}
+
+const suma = (n1, n2) => n1 + n2
+const resta = (n1, n2) => n1 - n2
+const iva = valor => valor * 0.21
+
+let precioProducto = 3000
+let precioProducto2 = 1500
+let descuento = 500
+
+let precioIva = resta(suma(precioProducto, iva(precioProducto)), descuento);
+let precioIva2 = resta(suma(precioProducto, iva(precioProducto2)), descuento);
+
+let alerta = alert("El total del producto es " + operacion + precioIva)
